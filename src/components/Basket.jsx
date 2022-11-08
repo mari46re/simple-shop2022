63212e2f44281d77f5bd9b743caac9803052fc94
Marikka Katrina Højen Jørgensen <mari46re@stud.kea.dk>
2022-11-08 13:07:45 +0100

    cart introduced

---
+++ src/components/Basket.jsx
@@ -0,0 +1,18 @@
+function Basket(props) {
+  return (
+    <section className="Basket">
+      <ul>
+        {props.cart.map((item) => (
+          <li>
+            {item.productdisplayname} x {item.amount},{" "}
+            {item.amount * item.price}
+          </li>
+        ))}
+        {/* <li>SomeProduct x 3, 1234,-</li> */}
+      </ul>
+      <button>Buy now</button>
+    </section>
+  );
+}
+
+export default Basket;
---
+++ src/components/Product.jsx
@@ -0,0 +1,36 @@
+function Product(props) {
+  function add() {
+    props.addToCart(props.data);
+  }
+  return (
+    <article className="Product">
+      <h2>{props.data.productdisplayname}</h2>
+      <p>{props.data.price},-</p>
+      <img
+        src={`https://kea-alt-del.dk/t7/images/webp/640/${props.data.id}.webp`}
+        alt={props.data.productdisplayname}
+      />
+      <button onClick={add}>Add to basket</button>
+    </article>
+  );
+}
+
+/*
+
+{
+    "id": 1163,
+    "gender": "Men",
+    "category": "Apparel",
+    "subcategory": "Topwear",
+    "articletype": "Tshirts",
+    "season": "Summer",
+    "productionyear": 2011,
+    "usagetype": "Sports",
+    "productdisplayname": "Sahara Team India Fanwear Round Neck Jersey",
+    "price": 895,
+    "discount": null,
+    "brandname": "Nike",
+    "soldout": 0
+  }
+*/
+export default Product;
