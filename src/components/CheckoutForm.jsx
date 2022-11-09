520bc395453f52abb4d599ce77dabfbe291e07e0
Marikka Katrina Højen Jørgensen <mari46re@stud.kea.dk>
2022-11-09 14:22:49 +0100

    thank you works

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,36 +1,53 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 import { insertOrder } from "../modules/db";
 
 function CheckoutForm(props) {
   const theForm = useRef();
+  const [paymentCompleted, setPaymentCompleted] = useState(false);
 
-  function submit(e) {
+  async function submit(e) {
     e.preventDefault();
-
-    insertOrder({
+    const response = await insertOrder({
       name: theForm.current.elements.name.value,
       email: theForm.current.elements.email.value,
       address: theForm.current.elements.address.value,
       basket: props.cart,
     });
+
+    //If response it not null etc and it has a length:
+    //I am assuming that it is an array
+    if (response && response.length) {
+      //When we have a response from server, we set the payment 2 b completed!
+      setPaymentCompleted(true);
+    }
+
+    console.log(paymentCompleted);
   }
+
   return (
-    <form onSubmit={submit} ref={theForm}>
-      <div className="form-control">
-        <label htmlFor="form-name">Name</label>
-        <input required type="text" name="name" id="form-name" />
-      </div>
-      <div className="form-control">
-        <label htmlFor="form-email">E-mail</label>
-        <input required type="email" name="email" id="form-email" />
-      </div>
-      <div className="form-control">
-        <label htmlFor="form-address">Address</label>
-        <textarea required name="address" id="form-address" />
-      </div>
-      <button>Pay</button>
-    </form>
+    <>
+      {paymentCompleted ? ( // If the payment is completed, return THANK
+        <p>THANK YOU</p>
+      ) : (
+        // Else, return the form
+        <form onSubmit={submit} ref={theForm}>
+          <div className="form-control">
+            <label htmlFor="form-name">Name</label>
+            <input required type="text" name="name" id="form-name" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="form-email">E-mail</label>
+            <input required type="email" name="email" id="form-email" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="form-address">Address</label>
+            <textarea required name="address" id="form-address" />
+          </div>
+          <button>Pay</button>
+        </form>
+      )}
+    </>
   );
 }
 
--- src/modules/db.js
+++ src/modules/db.js
@@ -1,18 +1,16 @@
-export function insertOrder(payload) {
+export async function insertOrder(payload) {
   const key =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVnYmRlbG9vaXZhZXV0cWFydXBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc5MTI5MjMsImV4cCI6MTk4MzQ4ODkyM30.6f8KCtL4Rp20SfePL134NB_mEAms-aNRp-ZgXvm1sk8";
   const url = "https://ugbdelooivaeutqarupo.supabase.co";
-  fetch(url + "/rest/v1/simpleshop", {
+  const res = await fetch(url + "/rest/v1/simpleshop", {
     method: "POST",
     headers: {
-      apikey:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVnYmRlbG9vaXZhZXV0cWFydXBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc5MTI5MjMsImV4cCI6MTk4MzQ4ODkyM30.6f8KCtL4Rp20SfePL134NB_mEAms-aNRp-ZgXvm1sk8",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVnYmRlbG9vaXZhZXV0cWFydXBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc5MTI5MjMsImV4cCI6MTk4MzQ4ODkyM30.6f8KCtL4Rp20SfePL134NB_mEAms-aNRp-ZgXvm1sk8",
+      apikey: key,
+      Authorization: `Bearer ${key}`,
       "Content-Type": "application/json",
+      Prefer: "return=representation",
     },
     body: JSON.stringify(payload),
-  })
-    .then()
-    .then();
+  });
+  return await res.json();
 }
